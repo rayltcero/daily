6ab0cd36e3ae7c7631e82046b493f8947e735c7f
DESKTOP-GF1J8F9\German <ray_jr@hotmail.es>
2024-09-13 15:06:16 -0400

    chore: add tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{css,xml,html,vue,svelte,ts,tsx}'],
+  content: ['./src/**/*.{css,xml,html,vue,svelte,ts,tsx}'],
   // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
   darkMode: ['class', '.ns-dark'],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['Playfair Display', 'PlayfairDisplay-Regular', 'PlayfairDisplay-Bold', 'Playfair Display Medium', 'PlayfairDisplay-Medium'],
+      sans: ['Montserrat', 'Montserrat-Regular', 'Montserrat Thin', 'Montserrat-Thin', 'Montserrat Medium', 'Montserrat-Medium', 'Montserrat-Bold'],
+    },
+    extend: {
+      colors: {
+        'primary': '#EDF2F4',
+        'secondary': '#2B2D42',
+        'accent': '#8D99AE'
+      }
+
+    },
   },
   plugins: [
     /**
