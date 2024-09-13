6884834f758b3f06ca1ebc2a86ab3e7b63f2e9d4
DESKTOP-GF1J8F9\German <ray_jr@hotmail.es>
2024-09-13 15:15:38 -0400

    feat: add initial molecules components

---
+++ src/components/ui/molecules/Calendar.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { cn } from "@/utils/cn";
+import { isAndroid } from "@nativescript/core";
+
+export const Calendar: React.FC = (props) => {
+    const [week, setWeek] = useState([
+        { day: "Sun", date: "01" },
+        { day: "Mon", date: "02" },
+        { day: "Tue", date: "03" },
+        { day: "Wed", date: "04" },
+        { day: "Thu", date: "05" },
+        { day: "Fri", date: "06" },
+        { day: "Sat", date: "07" },
+        { day: "Sun", date: "08" }
+    ]);
+
+    const baseClassDate = "text-center text-slate-900 text-base font-bold rounded-full m-2 w-8 h-7 p-0.5";
+    const androidShadow = "0 10 15 -3 rgb(0 0 0 / 0.1), 0 4 6 -4 rgb(0 0 0 / 0.1)";
+    const iosShadow = "0 10 15 -3 rgb(0 0 0 / 0.2), 0 4 6 -4 rgb(0 0 0 / 0.2)";
+
+    return (
+        <scrollView
+            orientation="horizontal"
+            scrollBarIndicatorVisible={false}
+            {...props}
+        >
+            <flexboxLayout
+                className="py-2"
+            >
+                {week.map((day, index) => (
+                    <flexboxLayout
+                        key={index}
+                        className={cn("mr-2 flex-col shrink grow w-11 h-20 justify-center", {
+                            "ml-2": index === 0,
+                            "mr-4": index === week.length - 1,
+                            "bg-slate-200 rounded-full": index === 3
+                        })}
+                        boxShadow={`${index === 3 ? isAndroid ? androidShadow : iosShadow : ""}`}
+                    >
+                        <label
+                            className="text-center text-slate-500 text-sm"
+                        >
+                            {day.day}
+                        </label>
+                        <label
+                            className={cn(baseClassDate, {
+                                "bg-white": index === 3
+                            })}
+                            verticalAlignment="middle"
+                        >
+                            {day.date}
+                        </label>
+                    </flexboxLayout>
+                ))}
+            </flexboxLayout>
+        </scrollView>
+    );
+}
---
+++ src/components/ui/molecules/Layout.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { isIOS } from "@nativescript/core";
+
+interface LayoutChildProps {
+    children: React.ReactNode;
+}
+
+const LayoutMainContent: React.FC<LayoutChildProps> = ({ children }) => {
+    return (
+        <contentView
+            row={0}
+            rowSpan={2}
+        >
+            {children}
+        </contentView>
+    )
+};
+const LayoutBottomContent: React.FC<LayoutChildProps> = ({ children }) => {
+    return (
+        <contentView
+            row={1}
+            col={0}
+            colSpan={5}
+        >
+            {children}
+        </contentView>
+    );
+};
+
+interface LayoutProps {
+    children: React.ReactNode;
+    mainContent: React.FC<LayoutChildProps>;
+    bottomContent: React.FC<LayoutChildProps>;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+    let mainContent: React.ReactElement | null = null;
+    let bottomContent: React.ReactElement | null = null;
+
+    React.Children.forEach(children, child => {
+        if (React.isValidElement(child)) {
+            if (child.type === LayoutMainContent) {
+                mainContent = child;
+            } else if (child.type === LayoutBottomContent) {
+                bottomContent = child;
+            }
+        }
+    });
+
+    return (
+        <gridLayout
+            rows="66.666666*, 33.333333*"
+            className="h-full"
+            backgroundColor="#EDF2F4"
+        >
+            {mainContent}
+
+            <gridLayout
+                row={1}
+                rows={"*, auto"}
+                columns="25*, 25*, 12.5*, 12.5*, 25*"
+                className='android:-mb-4 ios:-mb-9'
+            >
+                <svgView
+                    row={0}
+                    rowSpan={2}
+                    col={0}
+                    colSpan={ isIOS ? 3 : 2}
+                    src="~/assets/shape.svg"
+                    stretch="aspectFit"
+                    className="-mb-4"
+                />
+                {bottomContent}
+            </gridLayout>
+        </gridLayout>
+    );
+};
+
+(Layout as any).mainContent = LayoutMainContent;
+(Layout as any).bottomContent = LayoutBottomContent;
+
+
