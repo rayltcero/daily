869a2c023fcc1880cc049417583a3810effae903
DESKTOP-GF1J8F9\German <ray_jr@hotmail.es>
2024-10-19 23:52:20 -0400

    feat: add avatar atom component

---
+++ src/components/ui/atoms/Avatar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { ViewAttributes, NSVElement } from 'react-nativescript';
+
+type AvatarSize = "sm" | "md" | "lg";
+
+interface AvatarProps extends ViewAttributes {
+    size?: AvatarSize;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({ size = "md", className, ...viewAttributes }) => {
+    const localClassName = cn("rounded-full", className, {
+        "h-10 w-10": size === "sm",
+        "h-12 w-12": size === "md",
+        "h-16 w-16": size === "lg"
+    });
+
+    return (
+        <ncImage
+            className={localClassName}
+            roundAsCircle={true}
+            aspectRatio={1}
+            {...viewAttributes}
+        />
+    );
+}
--- src/components/ui/atoms/index.ts
+++ src/components/ui/atoms/index.ts
@@ -3,3 +3,4 @@ export { Container } from "./Container";
 export { Input } from "./Input";
 export { FormLabel } from "./FormLabel";
 export { Button } from "./Button";
+export { Avatar } from "./Avatar";
