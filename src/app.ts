365ce53f331f94e404394c1ab860ee35c110a59b
DESKTOP-GF1J8F9\German <ray_jr@hotmail.es>
2024-10-31 18:24:48 -0400

    chore: add nativescript comunity image and fonticon pkgs

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,15 @@ import { MainStack } from './components/MainStack';
 import { registerElement } from "react-nativescript";
 import { SVGView } from '@nativescript-community/ui-svg';
 import { ViewAttributes } from 'react-nativescript';
+import { FontIconFactory, fonticon } from "nativescript-fonticon";
+import { knownFolders } from "@nativescript/core/file-system";
+import * as imageModule from '@nativescript-community/ui-image';
+
+FontIconFactory.debug = true;
+FontIconFactory.paths = {
+  fa: knownFolders.currentApp().getFile("./assets/css/fa.css").readTextSync(),
+};
+FontIconFactory.loadCss();
 
 // In NativeScript, the app.ts file is the entry point to your application. You
 // can use this file to perform app-level initialization, but the primary
@@ -14,17 +23,25 @@ import { ViewAttributes } from 'react-nativescript';
 // - false: only error logs.
 Object.defineProperty(global, '__DEV__', { value: false });
 
+// define global function/property for fonticon
+global.fonticon = fonticon;
+imageModule.initialize({ isDownsampleEnabled: true });
+
 registerElement("svgView", () => require('@nativescript-community/ui-svg').SVGView);
+registerElement("ncImage", () => require('@nativescript-community/ui-image').Img);
 
 type SVGViewAttributes = {
   src: string;
   stretch: string;
 }&ViewAttributes;
 
+type NCImageAttributes = typeof imageModule.Img & ViewAttributes;
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       svgView: ReactNativeScript.NativeScriptProps<SVGViewAttributes, SVGView>;
+      ncImage: ReactNativeScript.NativeScriptProps<NCImageAttributes>;
     }
   }
 }
